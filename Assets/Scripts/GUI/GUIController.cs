8197ca3d96aa2092585b74db43c306b207937f43
Filippo Sciamanna <filippo.sciamanna@mail.polimi.it>
2018-11-04 12:06:03 +0100

    Add GUI

---
+++ Assets/Scripts/GUI/GUIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIController : MonoBehaviour {
+	[Range(1,60)]
+    public int TimeDuration = 1;
+	public Slider ProgressBar;
+	
+    private float _timePassed=0f;
+    private float _timeSlot;
+	
+	void Start()
+	{
+	    _timeSlot=TimeDuration*60;
+	} 
+	// Update is called once per frame
+	void Update ()
+	{
+		_timePassed += Time.deltaTime;
+		if (_timePassed > _timeSlot)
+			_timePassed = 0;
+		
+		ProgressBar.value=CalculateProgress();
+		
+
+	}
+
+	float CalculateProgress()
+	{
+		return _timePassed / _timeSlot;
+	}
+}
