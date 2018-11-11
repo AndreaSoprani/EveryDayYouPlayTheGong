7f18d58dc76f13e66f9ba49fc95d7a7d10f58b25
Alby395 <alby395@gmail.com>
2018-11-11 19:41:09 +0100

    Added simple teleport
    Added a way to teleport between to point.
    Need to find a way to fade or create a transition.

---
+++ Assets/Scripts/Objects/Teleport.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Teleport Destination;
+    public Vector3 Offset;
+    
+    private bool _teleportActive = true;
+    private bool _needDeactivation;
+
+    private void Start()
+    {
+        _needDeactivation = !(Offset.sqrMagnitude > 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (_teleportActive)
+            {  
+                if(_needDeactivation)
+                    Destination.StopTeleport();
+              
+                other.gameObject.transform.position = Destination.transform.position + Offset;
+            }
+        }
+    }
+
+    private void StopTeleport()
+    {
+        _teleportActive = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        _teleportActive = true;
+    }
+}
