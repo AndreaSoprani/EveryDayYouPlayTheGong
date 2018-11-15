a3f55bc498f5332830a7b52187b3bc2c8886968f
Alby395 <alby395@gmail.com>
2018-11-15 17:44:38 +0100

    Added Sorting Layer switcher
    - added a way that allows to move sprites behind other sprites
    - modified player collider
    - added random tree

---
+++ Assets/Scripts/Objects/LayerSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSwitcher : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("LayerSwitch"))
+            other.GetComponentInParent<SpriteRenderer>().sortingLayerName = "CharacterBehind";
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("LayerSwitch"))
+            other.GetComponentInParent<SpriteRenderer>().sortingLayerName = "CharacterFront";
+    }
+}
