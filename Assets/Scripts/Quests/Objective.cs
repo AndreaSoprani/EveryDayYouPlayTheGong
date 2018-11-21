7e550bae8bdf2fc1b11650dc42838687343cd2c6
Andrea Soprani <andrea.soprani96@gmail.com>
2018-11-21 20:34:59 +0100

    Initiated quest system
    Quest class created
    Objective abstract class created

---
+++ Assets/Scripts/Quests/Objective.cs
@@ -0,0 +1,13 @@
+﻿namespace Quests
+{
+    /// <summary>
+    /// The Objective class contains the information for a single objective inside a quest.
+    /// </summary>
+    [System.Serializable]
+    public abstract class Objective
+    {
+        public string ObjectiveID; // Unique ID of the Objective.
+        public string Description; // Text description of the objective.
+        public bool Completed; 
+    }
+}
---
+++ Assets/Scripts/Quests/Quest.cs
@@ -0,0 +1,42 @@
+﻿namespace Quests
+{
+    /// <summary>
+    /// The Quest class contains the information for a single quest with all the relative objectives.
+    /// </summary>
+    [System.Serializable]
+    public class Quest
+    {
+
+        public string QuestID; // Unique ID of the Quest.
+        public string Description; // Text description of the Quest.
+        public Objective[] Objectives;
+
+        /// <summary>
+        /// Signals if all the objectives are completed.
+        /// </summary>
+        /// <returns>True if all objectives are completed, false if at least one is not.</returns>
+        public bool IsComplete()
+        {
+            for (int i = 0; i < Objectives.Length; i++)
+            {
+                if (!Objectives[i].Completed) return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns the first uncompleted objective.
+        /// </summary>
+        /// <returns>The first uncompleted objective, null if they are all complete.</returns>
+        public Objective NextObjective()
+        {
+            for (int i = 0; i < Objectives.Length; i++)
+            {
+                if (!Objectives[i].Completed) return Objectives[i];
+            }
+
+            return null;
+        }
+    }
+}
