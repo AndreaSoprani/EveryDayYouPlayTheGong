c7c0d4561141e46ed4ad973f7be4ee6a2b83f289
Andrea Soprani <andrea.soprani96@gmail.com>
2018-12-12 16:26:36 +0100

    doors can be locked after entering.

--- Assets/Scripts/Editor/TeleportEditor.cs
+++ Assets/Scripts/Editor/TeleportEditor.cs
@@ -5,10 +5,13 @@ using Utility;
 [CustomEditor (typeof(Teleport))]
  public class TeleportEditor : UnityEditor.Editor
 {
+ 
  public override void OnInspectorGUI()
  {
   Teleport myScript = (Teleport) target;
 
+  EditorGUILayout.LabelField("General", EditorStyles.boldLabel);
+  
   myScript.Destination = (Teleport) EditorGUILayout.ObjectField("Destination",myScript.Destination,typeof(Teleport),true);
   myScript.Offset = EditorGUILayout.Vector3Field("Offset", myScript.Offset);
   myScript.CanChangeMusic = EditorGUILayout.Toggle("Can change music", myScript.CanChangeMusic);
@@ -18,7 +21,18 @@ using Utility;
    myScript.MusicId =EditorGUILayout.TextField("Event name",myScript.MusicId);
   }
   
+  EditorGUILayout.LabelField("Close door after incoming teleport", EditorStyles.boldLabel);
+
+  myScript.CloseDoor = EditorGUILayout.Toggle("Close door", myScript.CloseDoor);
+  
+  if (myScript.CloseDoor)
+  {
+   myScript.DoorToClose = (Door) EditorGUILayout.ObjectField("Door", myScript.DoorToClose, typeof(Door), true);
+   myScript.CloseOnlyOnce = EditorGUILayout.Toggle("Close only once", myScript.CloseOnlyOnce);
+  }
+  
   EditorGUILayout.LabelField("Fade",EditorStyles.boldLabel);
+  
   myScript.TimeIn = EditorGUILayout.FloatField("Time in",myScript.TimeIn);
   myScript.Pause=EditorGUILayout.FloatField("Pause",myScript.Pause);
   myScript.TimeOut=EditorGUILayout.FloatField("Time out",myScript.TimeOut);
--- Assets/Scripts/Objects/Teleport.cs
+++ Assets/Scripts/Objects/Teleport.cs
@@ -10,7 +10,11 @@ public class Teleport : MonoBehaviour
     public bool CanChangeMusic;
     public string MusicId;
 
-    [Header("Fade")]
+    public bool CloseDoor;
+    public Door DoorToClose; // If the door related to the teleport has to be closed put it in the destination teleport.
+    public bool CloseOnlyOnce; // True if the door has to be closed just once in the game and left it open the other times.
+    private bool _closedOnce = false;
+
     public float TimeIn = 0.4f;
     public float Pause = 0.3f;
     public float TimeOut = 0.5f;
@@ -29,6 +33,7 @@ public class Teleport : MonoBehaviour
         if (_teleportActive && other.CompareTag("Player"))
         {
             StartCoroutine(ApplyTeleport(other));
+            Destination.CheckCloseDoor();
             if(_needDeactivation)
                 Destination.StopTeleport();
             if(CanChangeMusic)
@@ -66,5 +71,19 @@ public class Teleport : MonoBehaviour
         yield return StartCoroutine(CameraFade.Instance.FadeTo(TimeOut, 0f));
         
         //TODO Restart player movement
+    }
+
+    /// <summary>
+    /// Method used to closed the door related to the Teleport.
+    /// It is called by the origin teleport on the destination teleport.
+    /// </summary>
+    public void CheckCloseDoor()
+    {
+        if (DoorToClose == null) return;
+        if (CloseOnlyOnce && _closedOnce) return;
+        
+        DoorToClose.CloseDoor();
+        _closedOnce = true;
+
     }
 }
