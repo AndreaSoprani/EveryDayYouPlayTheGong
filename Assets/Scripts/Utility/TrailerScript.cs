7054d6fad6e2eea1df7308c350b94bcafb8278f3
Filippo Sciamanna <filippo.sciamanna@mail.polimi.it>
2019-01-06 10:27:35 +0100

    some trailer stuff

---
+++ Assets/Scripts/Utility/TrailerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerScript : MonoBehaviour
+{
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.J))
+			AudioManager.Instance.PlayMusic("Dungeon");
+		if(Input.GetKeyDown(KeyCode.Keypad1))
+			DayProgressionManager.Instance.Progress();
+		
+	}
+}
