f9d4eb171cdee32af92c625a2a3fd0f95d5a6ed6
Andrea Soprani <andrea.soprani96@gmail.com>
2019-02-13 17:22:49 +0100

    Splash fades completed

--- Assets/Scripts/MainMenu/SplashScript.cs
+++ Assets/Scripts/MainMenu/SplashScript.cs
@@ -6,9 +6,11 @@ public class SplashScript : MonoBehaviour
 {
 
 	public MainMenuController MenuController;
+	public Image Black;
 	public Image Team;
 	public Image Polimi;
 	public float OnScreenTime;
+	public float TransitionTime;
 
 	private void OnEnable()
 	{
@@ -17,13 +19,81 @@ public class SplashScript : MonoBehaviour
 
 	public IEnumerator StartupRoutine()
 	{
+		Black.enabled = true;
+		
+		Color tmp = Team.color;
+		tmp.a = 0;
+		Team.color = tmp;
 		Team.enabled = true;
+		
+		for (float t = 0f; t < 1.0f; t += Time.deltaTime / TransitionTime)
+		{
+			tmp.a = t;
+			Team.color = tmp;
+			yield return null;
+		}
+
+		tmp.a = 1;
+		Team.color = tmp;
+		
 		yield return new WaitForSeconds(OnScreenTime);
-		Polimi.enabled = true;
+		
+		for (float t = 1f; t > 0f; t -= Time.deltaTime / TransitionTime)
+		{
+			tmp.a = t;
+			Team.color = tmp;
+			yield return null;
+		}
+
+		tmp.a = 0;
+		Team.color = tmp;
+		
 		Team.enabled = false;
+
+		tmp = Polimi.color;
+		tmp.a = 0;
+		Polimi.color = tmp;
+		Polimi.enabled = true;
+		
+		for (float t = 0f; t < 1.0f; t += Time.deltaTime / TransitionTime)
+		{
+			tmp.a = t;
+			Polimi.color = tmp;
+			yield return null;
+		}
+
+		tmp.a = 1;
+		Polimi.color = tmp;
+		
 		yield return new WaitForSeconds(OnScreenTime);
-		MenuController.gameObject.SetActive(true);
+		
+		for (float t = 1f; t > 0f; t -= Time.deltaTime / TransitionTime)
+		{
+			tmp.a = t;
+			Polimi.color = tmp;
+			yield return null;
+		}
+
+		tmp.a = 0;
+		Polimi.color = tmp;
+		
 		Polimi.enabled = false;
+		
+		MenuController.gameObject.SetActive(true);
+		
+		tmp = Black.color;
+		
+		for (float t = 1f; t > 0f; t -= Time.deltaTime / TransitionTime)
+		{
+			tmp.a = t;
+			Black.color = tmp;
+			yield return null;
+		}
+		
+		MenuController.gameObject.SetActive(true);
+		
+		Black.enabled = false;
+		
 		gameObject.SetActive(false);
 		
 	}
